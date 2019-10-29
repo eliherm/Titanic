a2c8794d3dba2c8124d5c40d37917a2131d9e040
Bennet Montgomery <17blm1@caslab.queensu.ca>
2019-10-29 16:47:43 -0400

    physics.h edited online with Bitbucket

--- titanic_eclipse/src/physics.h
+++ titanic_eclipse/src/physics.h
@@ -2,39 +2,38 @@
  * Header class for physics engine
  */
 #pragma once
-
+ 
 #include <vector>
-
+ 
 class object {
 public:
-	object(const int& xpos, const int& ypos, const float& gravity);
-	int getXCoord();
-	int getYCoord();
-	void setCoord(const int& newx, const &int newy);
-	float getSpeed();
-	void setSpeed(const float& newval);
-	float getDir();
-	void setDir(const float& newval);
-	float getGrav();
-	void setGrav(const float& newval);
+    object(const int& xpos, const int& ypos, const float& gravity);
+    int getXCoord();
+    int getYCoord();
+    void setCoord(const int& newx, const &int newy);
+    float getSpeed();
+    void setSpeed(const float& newval);
+    float getDir();
+    void setDir(const float& newval);
+    float getGrav();
+    void setGrav(const float& newval);
 private:
-	int xcoord;
-	int ycoord;
-	float speed; //speed in pixels/s
-	float direction; //direction of movement in radii
-	float gravity; //acceleration due to gravity in pixels/s^2
+    int xcoord;
+    int ycoord;
+    float speed; //speed in pixels/s
+    float direction; //direction of movement in radii
+    float gravity; //acceleration due to gravity in pixels/s^2
 };
-
+ 
 class physicsEngine {
 public:
-	void initLevel(object player, object door, object water, vector<object> platforms);
-	void updateObjects(const vector<int>& keypresses); //needs to take whatever the data format for key press input is
-	vector<object> getState();
+    physicsEngine(object player, object door, object water, vector<object> platforms);
+    void updateObjects(const vector<int>& keypresses); //needs to take whatever the data format for key press input is
+    vector<object> getState();
 private:
-	float** checkCollision(object obj1, object obj2);
-	object player;
-	object door;
-	object water;
-	vector<object> platforms;
+    float** checkCollision(object obj1, object obj2);
+    object player;
+    object door;
+    object water;
+    vector<object> platforms;
 };
-
